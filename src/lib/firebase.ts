a84a466e7586adfb1180c8c75ddee8c17942dffa
Nyai Tarsih <ntarsih32@gmail.com>
2025-09-29 19:29:30 +0000

    buatkan env databasenya

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore, query } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUQlkRsahgO4u6JDEEimkEtHMeMCii1dY",
-  authDomain: "studio-744538801-63042.firebaseapp.com",
-  projectId: "studio-744538801-63042",
-  storageBucket: "studio-744538801-63042.appspot.com",
-  messagingSenderId: "176230956015",
-  appId: "1:176230956015:web:cbb150d81a07cc4dd4d41a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
