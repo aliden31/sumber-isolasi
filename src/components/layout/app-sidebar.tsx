4b7b00bbec5e40475ff2c00f251ea7758b8b0f40
Nyai Tarsih <ntarsih32@gmail.com>
2025-09-29 15:37:40 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -52,7 +52,7 @@ export function AppSidebar() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref legacyBehavior>
+              <Link href={item.href} passHref>
                 <SidebarMenuButton
                   as="a"
                   isActive={pathname === item.href}
