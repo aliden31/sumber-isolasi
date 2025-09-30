972233a706538e38deba6704d2a5cc514e654515
Nyai Tarsih <ntarsih32@gmail.com>
2025-09-30 10:03:17 +0000

    Lanjutkan

--- src/app/(app)/stock/transfer/page.tsx
+++ src/app/(app)/stock/transfer/page.tsx
@@ -6,7 +6,7 @@ export default function StockTransferPage() {
   return (
      <PlaceholderPage 
         title="Transfer Stok Antar Gudang"
-        description="Fitur ini digunakan untuk mencatat dan melacak perpindahan stok barang dari satu lokasi gudang ke lokasi lainnya. Sistem akan memastikan kuantitas stok di gudang asal berkurang dan di gudang tujuan bertambah secara akurat, menjaga integritas data inventaris Anda."
+        description="Fitur ini akan digunakan untuk mencatat dan melacak perpindahan stok barang dari satu lokasi gudang ke lokasi lainnya. Alur kerjanya akan melibatkan pemilihan gudang asal dan tujuan, pemilihan produk beserta kuantitasnya, dan konfirmasi transfer yang akan secara otomatis mengurangi stok di gudang asal dan menambahkannya di gudang tujuan untuk menjaga integritas data inventaris Anda."
     />
   );
 }
